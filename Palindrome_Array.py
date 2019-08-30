78fab17d66ebe95f5cd8f21a2dedb8d25cbe5a47
Hats <monstersho@gmail.com>
2019-08-30 07:56:38 +0100

    Added exercise 1

---
+++ Palindrome_Array.py
@@ -0,0 +1,26 @@
+"""
+https://practice.geeksforgeeks.org/problems/palindromic-array/1
+Given a Integer array A[] of n elements.
+Your task is to complete the function PalinArray.
+Which will return 1 if all the elements of the Array are palindrome otherwise it will return 0.
+
+Input:
+number of test cases
+test case x size of the array
+the array of potential palindromes in case x
+"""
+
+
+def palin_array():
+    #  get the data from the user
+    print("HELLO")
+    pal = input("Enter your potential palindrome set:")
+    print("You input " + pal)
+
+
+#  break up and put in array
+#  check it
+#  tell the user
+
+
+palin_array()
